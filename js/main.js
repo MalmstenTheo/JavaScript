36f07a3886f83b8103d938481b4ca3297d51a85f
MalsmtenTheo <theo.m1590@gmail.com>
2023-05-18 15:36:20 -0300

    Correccion a ecuacion interesSimple

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let monto = parseInt(prompt("Ingrese el monto incial (ARS): "))
 let plazo = parseInt(prompt(" Plazo en dias:\n 30 a 360"))
-const tna = monto * 0.097
+const tna = 97
 
 function interesSimple(monto, tna, plazo) {
     return (monto * tna * plazo) / (100 * 365)
